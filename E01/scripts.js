fb1ee0dff16bedddbcc79c22ea9bc5ff9f77cc0d
Mario Albano <marioalbanon@gmail.com>
2022-07-08 15:22:39 -0300

    Feat: primeiro exercicio lógica

---
+++ E01/scripts.js
@@ -0,0 +1,28 @@
+//Crie um algoritmo que transforme as notas do sistema numério para sistema de notas em caracteres tipo A B C;
+
+function categorizandoNotas(notaAluno) {
+
+    if (notaAluno >= 90) {
+        return "Nota A";
+    }
+    else if (notaAluno >= 80 && notaAluno <= 89) {
+        return "Nota B";
+    }
+    else if (notaAluno > 70 && notaAluno < 79) {
+        return "Nota C";
+    }
+    else if (notaAluno > 60 && notaAluno < 69) {
+        return "Nota D";
+    }
+    else if (notaAluno < 60) {
+        return "Nota F";
+    }
+    else {
+        return "Nota Inválida, verifique novamente por favor";
+    }
+}
+console.log(categorizandoNotas(100));
+console.log(categorizandoNotas(89));
+console.log(categorizandoNotas(72));
+console.log(categorizandoNotas(61));
+console.log(categorizandoNotas(0));
