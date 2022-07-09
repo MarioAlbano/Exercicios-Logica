95755b2f5863d1586510d8bf902b2a3b078de5ba
Mario Albano <marioalbanon@gmail.com>
2022-07-09 15:25:17 -0300

    Feat: concluido exercicio conversos celsius fahrenheit

--- E03/scripts.js
+++ E03/scripts.js
@@ -1,3 +1,31 @@
 /*Celsius em fahrenheit
 
-  Crie uma função que receba uma sting em celsius ou */
+  Crie uma função que receba uma string em celsius ou fahrenheit e faça a transformação de uma unidade para outra
+*/
+function conversorMedidasC(quantidade) {
+    let valor = parseFloat(quantidade);
+    let resultado = ((valor * 9 / 5) + 32);
+    return `${quantidade} em Celsius é igual a ${resultado} Fahrenheit.`;
+}
+
+function conversorMedidasF(valorF) {
+    let valor = parseFloat(valorF)
+    let resultado = ((valor - 32) * (5 / 9));
+    return `${valorF} em Fahrenheit é igual a ${resultado} Celsius.`;
+}
+
+function conversor(quantidadeValor, tipo) {
+    if (tipo === "c" || "C") {
+        let quantidade = quantidadeValor;
+        console.log(conversorMedidasC(quantidade));
+    }
+    else {
+        let quantidade = quantidadeValor;
+        console.log(conversorMedidasF(quantidade));
+    }
+
+}
+conversor("40", "c");
+conversor("100", "C");
+conversor("50", "F");
+conversor("500", "f");
